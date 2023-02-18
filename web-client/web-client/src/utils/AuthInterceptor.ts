96af0892a4eeb186c3dcfabc92926f92ae64210f
Emad Khatrush <qwe.emad@hotmail.com>
2023-02-18 21:24:58 +0300

    .

--- web-client/web-client/src/utils/AuthInterceptor.ts
+++
@@ -1,8 +0,0 @@
-import { base } from '../api';
-
-export const addAuthInterceptor = (token: string) => {
-  base.axios.interceptors.request.use((config: any) => {
-		config.headers.authorization = `Bearer ${token}`;
-		return config;
-	});	
-};
